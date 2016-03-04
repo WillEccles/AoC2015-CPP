38027a3d25b5a3f064899894da3b8655e290a1b5
Will Eccles <WillEccles@users.noreply.github.com>
2016-03-04 10:59:10 -0500

    Update Day1.cpp

--- Advent of Code 2015/Day1.cpp
+++ Advent of Code 2015/Day1.cpp
@@ -9,9 +9,9 @@ using namespace std;
 // since I already did 1-3 in Java, this is going to be as minimal as possible, though not a one-liner unless possible
 
 void Day1::run() {
-	int f;
-	string l;
-	for (string p : getline(ifstream("Day 1 Input.txt"), l)) {
-
+	int f = 0;
+	char line[] = getline(fstream("Day 1 Input.txt"));
+	for (char c : l) {
+		
 	}
 }
